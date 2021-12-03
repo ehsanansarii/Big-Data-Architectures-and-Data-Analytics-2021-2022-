d30dbd948fc4f09f6f78a498d3d7756807c5920e
Ehsan Ansari Nejad <s288903@studenti.polito.it>
2021-12-03 16:34:09 +0100

    lab2

---
+++ Lab2/Lab2_Skeleton/src/main/java/it/polito/bigdata/hadoop/lab/MapperBigData.java
@@ -0,0 +1,35 @@
+package it.polito.bigdata.hadoop.lab;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * Lab  - Mapper
+ */
+
+/* Set the proper data types for the (key,value) pairs */
+class MapperBigData extends Mapper<
+                    LongWritable, // Input key type
+                    Text,         // Input value type
+                    Text,         // Output key type
+                    IntWritable> {// Output value type
+    
+    protected void map(
+            LongWritable key,   // Input key type
+            Text value,         // Input value type
+            Context context) throws IOException, InterruptedException {
+
+    		/* Implement the map method */ 
+    	String words[] = value.toString().split("\\s+");
+    	
+    for(int i=0; i<words.length; i++) {
+    	if(words[i].toLowerCase().startsWith("ho")) {
+    	context.write(new Text(words[i]),new IntWritable(1));
+    	}
+    }
+    }
+}
